0949f18f2488e2efd460cac4ce9d7ab3db168420
hyunjun <hyunjun@DESKTOP-CGTR5BN>
2023-03-04 20:58:31 +0900

    행렬 테두리 회전하기

--- src/code/Main.java
+++ src/code/Main.java
@@ -2,6 +2,14 @@ package code;
 
 public class Main {
 	public static void main(String[] args) {
+		MatrixEdge m = new MatrixEdge();
+		int rows = 6;
+		int columns = 6;
+		int[][] queries = {{2,2,5,4},{3,3,6,6},{5,1,6,3}};
+		int[] res = m.solution(rows, columns, queries);
 		
+		for(int i : res) {
+			System.out.println(i);
+		}
 	}
 }
