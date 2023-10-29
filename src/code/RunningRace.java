23a3e01de7fa40e14f2e3821c3c75ade391e999c
hyunjun <hyunjun@DESKTOP-CGTR5BN>
2023-10-29 21:10:25 +0900

    달리기 경주

---
+++ src/code/RunningRace.java
@@ -0,0 +1,37 @@
+package code;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RunningRace {
+
+	public static void main(String[] args) {
+		String[] players = {"mumu", "soe", "poe", "kai", "mine"};
+		String[] callings = {"kai", "kai", "mine", "mine"};
+		
+		solution(players, callings);
+	}
+	
+	public static String[] solution(String[] players, String[] callings) {
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        
+        int i = 0;
+        for(String p : players) {
+        	map.put(p, i++);
+        }
+        
+        for(String c : callings) {
+        	int idx = map.get(c);
+        	int raceIdx = idx-1;
+        	map.put(c, raceIdx);
+        	
+        	String behindPlayer = players[raceIdx];
+        	map.put(behindPlayer, idx);
+        	
+        	players[raceIdx] = c;
+        	players[idx] = behindPlayer;
+        }
+        
+        return players;
+    }
+}
