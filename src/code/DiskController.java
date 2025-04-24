4fb74f7012ec927f5192a3133304dfc475c34415
hyunjun <hyunjun5681@kakao.com>
2025-04-25 00:58:11 +0900

    디스크 컨트롤러 주석 추가

--- src/code/DiskController.java
+++ src/code/DiskController.java
@@ -1,7 +1,13 @@
 package code;
 
+/**
+ * 디스크 컨트롤러
+ * https://school.programmers.co.kr/learn/courses/30/lessons/42627
+ *
+ * @author hyunjun
+ */
+
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class DiskController {
