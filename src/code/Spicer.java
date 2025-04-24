b7c360cfa0293a07936d9916d85a9c2c382fbe2a
hyunjun <hyunjun5681@kakao.com>
2025-04-24 19:23:31 +0900

    더 맵게 수정

--- src/code/Spicer.java
+++ src/code/Spicer.java
@@ -29,32 +29,37 @@ public class Spicer {
 
 	public static int solution(int[] scoville, int K) {
 		int answer = 0;
+		int val = Arrays.stream(scoville).min().orElse(0);
+
+		if(val >= K){	// 현재 가장 맵지 않은 스코빌 지수가 K 이상인 경우
+			return 0;
+		}
 
 		PriorityQueue<Integer> queue = new PriorityQueue<>();
 
-		// 배열 -> 우선순위 큐 저장
-		for(int sc : scoville){
-			queue.add(sc);
-		}
+		Arrays.stream(scoville)
+			.forEach(item -> {	// 배열 -> 우선순위 큐 저장
+				queue.add(item);
+			});
 
-		// 스코빌 지수 탐색
-		while(!queue.isEmpty()){
-			if(queue.peek() >= K){  // 제일 작은 스코빌 지수가 K 이상인 경우 종료
+		while (!queue.isEmpty()) {
+			if(queue.peek() >= K){	// 제일 작은 스코빌 지수가 K 이상인 경우 종료
 				break;
-			} else {    // 제일 작은 스코빌 지수가 K 미만인 경우
-				if(queue.size() < 2){   // 우선순위 큐 사이즈가 2미만인 경우 -1 리턴
+			} else {	// 제일 작은 스코빌 지수가 K 미만인 경우
+				if(queue.size() < 2){	// 우선순위 큐 사이즈가 2미만인 경우 -1 리턴
 					answer = -1;
+					break;
 				}
 
 				// 새로운 스코빌 지수 음식 추가
-				int firstSc = queue.poll();
-				int secondSc = queue.poll();
-				int newSc = firstSc + secondSc*2;
-				queue.add(newSc);
+				Integer s1 = queue.poll();
+				Integer s2 = queue.poll();
+				queue.add(s1+(s2*2));
+
 				answer++;
 			}
 		}
 
 		return answer;
-    }
+	}
 }
